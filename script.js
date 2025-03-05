83849ce7e40d45a6f20f9b81b7b24a17b02a68c2
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-05 14:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn = document.querySelector(".increment");
+const output = document.querySelector(".output");
+
+let count = 0;
+btn.addEventListener('click', ()=>{
+	count++;
+	alert(count);
+	output.innerText = count;
+})
